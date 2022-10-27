e3f5e0eedf0322a13b64b57d4a77f536ffc19e4b
yuxiaozzzzz <1726009889@qq.com>
2022-10-27 22:05:57 +0800

    second

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,9 @@ public class test {
     public static void main(String[] args) {
         for(int i = 0; i < 10; i ++){
             System.out.println(i);
+
+            int fjsfsaofils;
+            System.out.println(414134);
         }
     }
 }
